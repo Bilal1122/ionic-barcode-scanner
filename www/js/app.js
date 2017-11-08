9ae5e44bbba5a36f290c27996d0baac3d692c8ae
Bilal Tariq <bilal-tariq@live.com>
2017-11-08 10:46:43 +0800

    ionic barcode reader project started, added barcode scanner and rough flow

--- www/js/app.js
+++ www/js/app.js
@@ -4,21 +4,23 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+var db = null;
+var module = angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaSQLite) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    db = window.openDatabase("Project.db", "1.0", "Project", 1);
+    $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS item (id integer primary key, name text, description text, expiry_date date)");
   });
 })
 
@@ -69,5 +71,36 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/browse');
+});
+
+module.controller("ExampleController", function($scope, $cordovaBarcodeScanner, $cordovaSQLite) {
+  var scope = $scope
+  // $scope.getData = function(id) {
+  //   var query = "SELECT name, description FROM item WHERE id = ?";
+  //   var run = $cordovaSQLite.execute(db, query, [id]).then(function(result) {
+  //     if(result.rows.length > 0) {
+  //       alert("SELECTED -> " + result.rows.item(0).name);
+  //     } else {
+  //       alert("No results found");
+  //     }
+  //   }, function (error) {
+  //       console.error(error);
+  //   });
+  // };
+
+  $scope.scanBarcode = function() {
+    $cordovaBarcodeScanner.scan().then(function(imageData) {
+      alert(imageData.text);
+      // var query = "INSERT INTO item (name, description) VALUES (?,?)";
+      // $cordovaSQLite.execute(db, query, [imageData.text, imageData.text]).then(function(result) {
+      //   scope.getData(result.insertId);
+      // }, function (err) {
+      //   alert("something went wrong");
+      //   alert(err);
+      // });
+    }, function(error) {
+      console.log("An error happened -> " + error);
+    });
+  };
 });
